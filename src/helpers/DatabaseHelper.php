561cb7609c90e51a94f662409d3136911ee7c49f
Daniel Kipp <daniel.kipp@gmail.com>
2024-08-22 11:18:31 +0200

    formating change

--- src/helpers/DatabaseHelper.php
+++ src/helpers/DatabaseHelper.php
@@ -16,23 +16,23 @@ class DatabaseHelper {
 	}
 
 	public static function prepareAndExecute($conn, $sql, $params) {
-                // Die SQL-Abfrage vorbereiten
-                $stmt = $conn->prepare($sql);
+        // Die SQL-Abfrage vorbereiten
+        $stmt = $conn->prepare($sql);
 
-                // Parameter an die vorbereitete Anweisung binden
-                $stmt->bind_param(...$params);
+        // Parameter an die vorbereitete Anweisung binden
+        $stmt->bind_param(...$params);
 
-                // Die vorbereitete Anweisung ausführen
-                $stmt->execute();
+        // Die vorbereitete Anweisung ausführen
+        $stmt->execute();
 
-                // Das Ergebnis abrufen
-                $result = $stmt->get_result();
+        // Das Ergebnis abrufen
+        $result = $stmt->get_result();
 
-                // Ressourcen freigeben
-                $stmt->close();
-                $conn->close();
+        // Ressourcen freigeben
+        $stmt->close();
+        $conn->close();
 
-                return $result;
+        return $result;
     }
 }
 ?>
